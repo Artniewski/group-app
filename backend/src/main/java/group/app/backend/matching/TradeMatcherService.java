523fc71d42d1937db62f0040f48fffd547bb99cd
artniewski <artniewski@gmail.com>
2022-11-29 19:02:15 +0100

    Alpha version of algorithm

--- backend/src/main/java/group/app/backend/matching/TradeMatcherService.java
+++ backend/src/main/java/group/app/backend/matching/TradeMatcherService.java
@@ -1,103 +1,28 @@
 package group.app.backend.matching;
 
-import static org.jgrapht.alg.cycle.Cycles.simpleCycleToGraphPath;
+import static group.app.backend.matching.PathsCyclesUtil.getSimpleCycles;
+import static group.app.backend.matching.PathsCyclesUtil.mapCyclesToPaths;
+import static group.app.backend.matching.TradeGraph.buildGraph;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
-import org.jgrapht.alg.cycle.SzwarcfiterLauerSimpleCycles;
-import org.jgrapht.alg.shortestpath.AllDirectedPaths;
 import org.jgrapht.graph.DirectedMultigraph;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TradeMatcherService {
 
-    public Set<GraphPath<String, TradeEdge>> findCyclesAsPaths(List<User> users) {
+    public List<GraphPath<String, TradeEdge>> findCyclesAsPaths(List<User> users) {
         DirectedMultigraph<String, TradeEdge> graph = buildGraph(users);
         List<List<String>> cycles = getSimpleCycles(graph);
-        return mapCyclesToPaths(graph, cycles);
-    }
+        Set<GraphPath<String, TradeEdge>> graphPaths = mapCyclesToPaths(graph, cycles);
+        return graphPaths.stream()
+                .sorted(Comparator.comparingInt(GraphPath::getLength))
+                .toList();
 
-    private static Set<GraphPath<String, TradeEdge>> mapCyclesToPaths(DirectedMultigraph<String, TradeEdge> graph, List<List<String>> cycles) {
-        AllDirectedPaths<String, TradeEdge> allDirectedPaths = new AllDirectedPaths<>(graph);
-        return cycles.stream()
-                .filter(cycle -> !cycle.isEmpty())
-                .map(cycle -> {
-                    String firstVertex = cycle.get(0);
-                    String lastVertex = cycle.get(cycle.size() - 1);
-                    List<GraphPath<String, TradeEdge>> paths = allDirectedPaths.getAllPaths(firstVertex, lastVertex, true, 100);
-                    Set<TradeEdge> connectingEdges = graph.getAllEdges(lastVertex, firstVertex);
-                    return paths.stream()
-                            .map(path -> connectingEdges.stream()
-                                    .map(edge -> addEdgeToPath(graph, path, edge))
-                                    .collect(Collectors.toSet()))
-                            .flatMap(Collection::stream)
-                            .collect(Collectors.toSet());
-                })
-                .flatMap(Collection::stream)
-                .collect(Collectors.toSet());
-    }
-
-    private static GraphPath<String, TradeEdge> addEdgeToPath(DirectedMultigraph<String, TradeEdge> graph, GraphPath<String, TradeEdge> path, TradeEdge edge) {
-        List<TradeEdge> tradeEdges = new ArrayList<>(path.getEdgeList());
-        tradeEdges.add(edge);
-        return simpleCycleToGraphPath(graph, tradeEdges);
-    }
-
-    private static List<List<String>> getSimpleCycles(DirectedMultigraph<String, TradeEdge> graph) {
-        return new SzwarcfiterLauerSimpleCycles<>(graph).findSimpleCycles();
-    }
-
-    private static DirectedMultigraph<String, TradeEdge> buildGraph(List<User> users) {
-        DirectedMultigraph<String, TradeEdge> graph = new DirectedMultigraph<>(TradeEdge.class);
-        addUserVertices(users, graph);
-        addEdges(graph, users, getAvailableTasks(users));
-        return graph;
-    }
-
-    private static void addEdges(DirectedMultigraph<String, TradeEdge> graph, List<User> users, Set<String> availableTasks) {
-        availableTasks.forEach(task -> {
-            Set<String> requesters = new HashSet<>();
-            Set<String> owners = new HashSet<>();
-            users.forEach(user -> {
-                if (user.getOwnedTasks().contains(task)) {
-                    owners.add(user.getUserId());
-                }
-                if (user.getWantedTasks().contains(task)) {
-                    requesters.add(user.getUserId());
-                }
-            });
-            owners.forEach(owner -> requesters.forEach(requester -> graph.addEdge(owner, requester, new TradeEdge(task))));
-        });
-    }
-
-    private static Set<String> getAvailableTasks(List<User> users) {
-        Set<String> allOwnedTasks = users.stream()
-                .map(User::getOwnedTasks)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toSet());
-
-        Set<String> allWantedTasks = users.stream()
-                .map(User::getWantedTasks)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toSet());
-
-        return allWantedTasks.stream()
-                .filter(allOwnedTasks::contains)
-                .collect(Collectors.toSet());
-    }
-
-    private static void addUserVertices(List<User> users, Graph<String, TradeEdge> graph) {
-        users.stream()
-                .map(User::getUserId)
-                .forEach(graph::addVertex);
     }
 
 }
--- backend/src/test/java/group/app/backend/matching/TradeMatcherServiceTest.java
+++ backend/src/test/java/group/app/backend/matching/TradeMatcherServiceTest.java
@@ -1,8 +1,11 @@
 package group.app.backend.matching;
 
+import static group.app.backend.matching.PathsCyclesUtil.getOptimalCycles;
+
 import java.util.List;
 import java.util.Set;
 
+import org.jgrapht.GraphPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,14 +20,14 @@ public class TradeMatcherServiceTest {
     public void shouldFindPair() {
         User user1 = User.builder()
                 .userId("user1")
-                .ownedTasks(Set.of("1","3"))
+                .ownedTasks(Set.of("1", "3"))
                 .wantedTasks(Set.of("2"))
                 .build();
 
         User user2 = User.builder()
                 .userId("user2")
                 .ownedTasks(Set.of("2"))
-                .wantedTasks(Set.of("1","3"))
+                .wantedTasks(Set.of("1", "3"))
                 .build();
 
         var cycles = service.findCyclesAsPaths(List.of(user1, user2));
@@ -35,30 +38,33 @@ public class TradeMatcherServiceTest {
     public void shouldFindTriple() {
         User user1 = User.builder()
                 .userId("A")
-                .ownedTasks(Set.of("1"))
-                .wantedTasks(Set.of("2","3","5"))
+                .ownedTasks(Set.of("1", "8"))
+                .wantedTasks(Set.of("2", "3", "5", "9"))
                 .build();
 
         User user2 = User.builder()
                 .userId("B")
-                .ownedTasks(Set.of("2", "3","4"))
-                .wantedTasks(Set.of("1"))
+                .ownedTasks(Set.of("2", "3", "4"))
+                .wantedTasks(Set.of("1", "8"))
                 .build();
 
         User user3 = User.builder()
                 .userId("C")
-                .ownedTasks(Set.of("5","6"))
-                .wantedTasks(Set.of("4","7"))
+                .ownedTasks(Set.of("5", "6"))
+                .wantedTasks(Set.of("4", "7"))
                 .build();
         User user4 = User.builder()
                 .userId("D")
-                .ownedTasks(Set.of("7"))
-                .wantedTasks(Set.of("4","6"))
+                .ownedTasks(Set.of("7", "9"))
+                .wantedTasks(Set.of("4", "6"))
                 .build();
 
-
-        var cycles = service.findCyclesAsPaths(List.of(user1, user2, user3,user4));
-        cycles.forEach(System.out::println);
+        var paths = service.findCyclesAsPaths(List.of(user1, user2, user3, user4));
+        paths.forEach(System.out::println);
+        System.out.println("#########");
+        Set<GraphPath<String, TradeEdge>> optimalCycles = getOptimalCycles(Set.of(), paths);
+        System.out.println("Optimal cycles: " + optimalCycles);
+        optimalCycles.forEach(System.out::println);
     }
 
 }
