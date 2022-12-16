ef755b73f78de81900db89c14873e33f5d728ad8
KacperMajkowski <kacper20015@o2.pl>
2022-12-16 16:06:18 +0100

    Added constructor to UserVertex

--- backend/src/main/java/group/app/backend/matching/UserVertex.java
+++ backend/src/main/java/group/app/backend/matching/UserVertex.java
@@ -3,13 +3,11 @@ package group.app.backend.matching;
 import java.util.HashSet;
 import java.util.Set;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @EqualsAndHashCode(exclude = {"ownedTasks","wantedTasks"})
 public class UserVertex {
@@ -18,9 +16,4 @@ public class UserVertex {
     Set<String> ownedTasks;
     Set<String> wantedTasks;
     
-    public UserVertex(String userId, Set<String> ownedTasks, Set<String> wantedTasks) {
-        this.userId = userId;
-        this.ownedTasks = ownedTasks;
-        this.wantedTasks = wantedTasks;
-    }
 }
