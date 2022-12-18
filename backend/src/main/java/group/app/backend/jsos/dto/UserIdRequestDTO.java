3a76ff118c08ad58dd535ec90aaa7f34fcff3438
artniewski <artniewski@gmail.com>
2022-12-18 13:04:23 +0100

    Add get userId endpoint

---
+++ backend/src/main/java/group/app/backend/jsos/dto/UserIdRequestDTO.java
@@ -0,0 +1,16 @@
+package group.app.backend.jsos.dto;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+public class UserIdRequestDTO {
+    String jsossessid;
+}
