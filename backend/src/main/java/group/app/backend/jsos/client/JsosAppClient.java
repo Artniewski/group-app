310a792dcfb08b0f4c384452d76cce7afd2a77db
artniewski <artniewski@gmail.com>
2022-12-18 13:03:56 +0100

    Move request dir

--- backend/src/main/java/group/app/backend/jsos/client/JsosAppClient.java
+++ backend/src/main/java/group/app/backend/jsos/client/JsosAppClient.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import group.app.backend.jsos.dto.AuthRequestDTO;
 import group.app.backend.jsos.dto.AuthResponseDTO;
+import group.app.backend.jsos.dto.UserIdRequestDTO;
+import group.app.backend.jsos.dto.UserIdResponseDTO;
 
 @FeignClient(value = "jsos-app", url = "localhost:2137")
 public interface JsosAppClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "/auth")
     AuthResponseDTO authenticate(@RequestBody AuthRequestDTO authRequest);
+
+    @RequestMapping(method = RequestMethod.POST, value = "/id")
+    UserIdResponseDTO getUserId(@RequestBody UserIdRequestDTO authRequest);
 
 }
