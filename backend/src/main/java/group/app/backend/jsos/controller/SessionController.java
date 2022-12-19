df8404d321726bdd410a47a0a7b9fd28b82fbab6
artniewski <artniewski@gmail.com>
2022-12-19 16:27:46 +0100

    Add CORS support

--- backend/src/main/java/group/app/backend/jsos/controller/SessionController.java
+++ backend/src/main/java/group/app/backend/jsos/controller/SessionController.java
@@ -1,24 +1,15 @@
 package group.app.backend.jsos.controller;
 
-import java.util.Set;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import group.app.backend.jsos.dto.AddTaskListRequestDTO;
-import group.app.backend.jsos.dto.AuthRequestDTO;
-import group.app.backend.jsos.dto.CourseDTO;
-import group.app.backend.jsos.dto.LoginDTO;
-import group.app.backend.jsos.dto.TasksDTO;
+import group.app.backend.jsos.dto.*;
 import group.app.backend.jsos.services.LoginService;
 import group.app.backend.jsos.services.SessionService;
 import group.app.backend.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
+
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/session")
